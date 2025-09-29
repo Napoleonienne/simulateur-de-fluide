b1c17c04c919119611d80406d056c69ac2f19013
guy <eliassaint.matthias@gmail.com>
2025-09-29 19:42:52 +0200

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
-#include <glm
+#include <iostream>
 
 
+const uint16_t h = 800, l = 600;
 
 int main()
 {
+    if (!glfwInit()){
+        std::cout << "opengl ne c'est pas chargé";
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6); 
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+
+    GLFWwindow* fenetre = glfwCreateWindow(l,h,"engine v1",NULL,NULL);
     
+
     
+
 
 
 
